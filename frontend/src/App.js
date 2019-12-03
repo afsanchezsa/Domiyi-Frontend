de5fabc2109719188c59bcf0a940edd7b24f2b59
unknown <afsanchezsa@unal.edu.co>
2019-12-03 15:05:57 -0500

    implemented sidebar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -69,7 +69,68 @@ class App extends React.Component {//We render a component  depending of the act
 
     render() {
         return (
-            <Router>
+<div >
+<div className ="wrapper">
+<nav id ="sidebar">
+<div className="sidebar-header">
+            <h3>Bootstrap Sidebar</h3>
+        </div>
+        <ul className="list-unstyled components">
+            <p>Dummy Heading</p>
+            <li className="active">
+                <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false" className="dropdown-toggle">Home</a>
+                <ul className="collapse list-unstyled" id="homeSubmenu">
+                    <li>
+                        <a href="#">Home 1</a>
+                    </li>
+                    <li>
+                        <a href="#">Home 2</a>
+                    </li>
+                    <li>
+                        <a href="#">Home 3</a>
+                    </li>
+                </ul>
+                </li>
+                <li>
+                <a href="#">About</a>
+            </li>
+            <li>
+                <a href="#pageSubmenu" data-toggle="collapse" aria-expanded="false" className="dropdown-toggle">Pages</a>
+                <ul className="collapse list-unstyled" id="pageSubmenu">
+                    <li>
+                        <a href="#">Page 1</a>
+                    </li>
+                    <li>
+                        <a href="#">Page 2</a>
+                    </li>
+                    <li>
+                        <a href="#">Page 3</a>
+                    </li>
+                </ul>
+            </li>
+            <li>
+                <a href="#">Portfolio</a>
+            </li>
+            <li>
+                <a href="#">Contact</a>
+            </li>
+        </ul>
+    </nav>
+<div id="content">
+
+
+<nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid">
+
+            <button type="button" id="sidebarCollapse" className="btn btn-info">
+                <i className="fas fa-align-left"></i>
+                <span>Toggle Sidebar</span>
+            </button>
+
+        </div>
+    </nav>
+<div className="container-fluid">
+<Router>
                 <Navbar/>
 
                 <Route path="/Orders" render={(props) => <Orders {...props}/>}/>
@@ -85,6 +146,15 @@ class App extends React.Component {//We render a component  depending of the act
                                                        idOrder={this.state.idOrder}/>}/>
                 <Route path="/Login" component={FormLogin}></Route>
             </Router>
+
+
+
+</div>
+    
+</div>
+</div>
+
+</div>
         )
 
     }
