edf7ceb99984f28658951ab8d4e8f074389e0ff0
sv1024 <vejars1024@gmail.com>
2019-12-06 01:22:53 -0500

    in process, filter by word

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -5,68 +5,83 @@ import {Link} from 'react-router-dom';
 const Navbar = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <div className="container-fluid">
+            <div className="container-fluid">
 
-            <button type="button" id="sidebarCollapse" className="btn btn-info">
-                <i className="fas fa-align-left"></i>
-                <span>Admin</span>
-            </button>
-            <button className="btn btn-dark d-inline-block d-lg-none ml-auto" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <i class="fas fa-align-justify"></i>
-            </button>
+                <button type="button" id="sidebarCollapse" className="btn btn-info">
+                    <i className="fas fa-align-left"></i>
+                    <span>Admin</span>
+                </button>
+                <button className="btn btn-dark d-inline-block d-lg-none ml-auto" type="button" data-toggle="collapse"
+                        data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                        aria-expanded="false" aria-label="Toggle navigation">
+                    <i class="fas fa-align-justify"></i>
+                </button>
 
-            <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul className="nav navbar-nav ml-auto">
-                    <li className="nav-item active">
-                    <Link className="nav-link" to='/Login'>Login</Link>
-                    </li>
-                    <li className="nav-item">
-                    <Link className="nav-link" to='/Products'>Productos</Link>
-                    </li>
-                    <li className="nav-item">
-                    <Link className="nav-link" to='/CompanyRegister'>Registro Compañia</Link>
-                    </li>
-                    <li className="nav-item">
-                    <Link className="nav-link" to='/UserRegister'>Registro Usuario</Link>
-                    </li>
-                    <li className="nav-item">
-                    <Link className="nav-link" to='/ProductRegister'>Registro Producto</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link className="nav-link" to='/Orders'>Ordenes Pendientes</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link className="nav-link" to={{pathname:'/CompaniesByCategory/1',state:{idCategory: 1}}}>Alimentos</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link className="nav-link" to={{pathname:'/CompaniesByCategory/2',state:{idCategory: 2}}}>Licores</Link>
-                    </li>
-                    <a className="navbar-brand" href="#">
-                        <Link className="nav-link" to='/Cart'>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="nav navbar-nav ml-auto">
+                        <li className="nav-item active">
+                            <Link className="nav-link" to='/Login'>Login</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to='/Products'>Productos</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to='/CompanyRegister'>Registro Compañia</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to='/UserRegister'>Registro Usuario</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to='/ProductRegister'>Registro Producto</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to='/Orders'>Ordenes Pendientes</Link>
+                        </li>
 
-                            <img src="https://previews.123rf.com/images/val2014/val20141603/val2014160300005/54302312-shopping-cart-icon.jpg" width="30"
-                                 height="30" className="d-inline-block align-top" alt="">
-                            </img>
-                        </Link>
-                    </a>
-                  
-                </ul>
+                        <li className="nav-item">
+                            <Link className="nav-link"
+                                  to={{pathname: '/CompaniesByCategory/1', state: {idCategory: 1}}}>Alimentos</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link"
+                                  to={{pathname: '/CompaniesByCategory/2', state: {idCategory: 2}}}>Licores</Link>
+                        </li>
+
+                        <a className="navbar-brand" href="#">
+                            <Link className="nav-link" to='/Cart'>
+
+                                <img
+                                    src="https://previews.123rf.com/images/val2014/val20141603/val2014160300005/54302312-shopping-cart-icon.jpg"
+                                    width="30"
+                                    height="30" className="d-inline-block align-top" alt="">
+                                </img>
+                            </Link>
+                        </a>
+
+                    </ul>
+                    <form className="form-inline my-2 my-lg-0">
+                        <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                        </input>
+                        <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+
+                    </form>
+                </div>
+
             </div>
-        </div>
-    </nav>
-     
+        </nav>
+
+
     )
 }
 export default Navbar;
 
 
 /*
-    
-    <nav className="navbar navbar-expand-lg navbar-ligth bg-ligth">
-            <div className="cotainer-fluid">
 
-            
-            
+    <nav className="navbar navbar-expand-lg navbar-ligth bg-ligth">
+        <div className="cotainer-fluid">
+
+
             <button className="navbar-toggler" type="button" data-toggle="collapse"
                     data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false"
                     aria-label="Toggle navigation">
@@ -79,9 +94,9 @@ export default Navbar;
 
                     </img>
                 </a>
-            
+
                 <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
-                <li className="nav-item">
+                    <li className="nav-item">
                         <Link className="nav-link" to='/Login'>Login</Link>
                     </li>
                     <li className="nav-item">
@@ -92,7 +107,7 @@ export default Navbar;
                     </li>
                     <li className="nav-item">
                     </li>
-                        <Link className="nav-link" to='/UserRegister'>Registro Usuario</Link>
+                    <Link className="nav-link" to='/UserRegister'>Registro Usuario</Link>
                     <li className="nav-item">
                         <Link className="nav-link" to='/ProductRegister'>Registro Producto</Link>
 
@@ -102,20 +117,19 @@ export default Navbar;
 
                     </li>
                 </ul>
-        
+
             </div>
-            
-            </div>
-        </nav>
-        
-    */ 
+
+        </div>
+    </nav>
+
+*/
 
 /*
-     <button type="button" id="sidebarCollapse" className="btn btn-outline-primary">
-                <i className="fas fa-align-left"></i>
-                <span>Administrar</span>
-            </button>
-       
+    <button type="button" id="sidebarCollapse" className="btn btn-outline-primary">
+        <i className="fas fa-align-left"></i>
+        <span>Administrar</span>
+    </button>
 
 
 */
