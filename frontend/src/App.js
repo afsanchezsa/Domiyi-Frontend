cdf06a3d38f5f9077c0810abdfc7b0fbf16a1b7d
sv1024 <vejars1024@gmail.com>
2019-12-13 16:11:22 -0500

    Selection by price working

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -23,7 +23,7 @@ import AdminProduct from './components/AdminProduct';
 import CompaniesByIdAdmin from "./components/CompaniesByIdAdmin";
 import SearchProduct from "./components/SearchProduct";
 import PriceRange from "./components/PriceRange";
-
+import ProductsByPriceSearch from "./components/ProductsByPriceSearch";
 class App extends React.Component {//We render a component  depending of the action of the user in the navbar
     state = {
         idOrder: null,
@@ -105,6 +105,7 @@ class App extends React.Component {//We render a component  depending of the act
                                 <Route path="/AdminProduct" component={AdminProduct}></Route>
                                 <Route path="/MyCompanies" component={CompaniesByIdAdmin}></Route>
                                 <Route path="/SelectionByPrice" component={PriceRange}></Route>
+                                <Route path="/ProductsByPriceSearch" component={ProductsByPriceSearch}></Route>
                                 <Route path="/ListProductToEdit"
                                        render={(props) => <ListProductToEdit {...props} idCompany={1}
                                                                              goToAddProduct={this.goToAddProduct}
--- frontend/src/components/PriceRange.js
+++ frontend/src/components/PriceRange.js
@@ -1,25 +1,100 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import InputRange from 'react-input-range';
+//'/products/price'
 
-class PriceRange extends React.Component {
+import React, {Component} from 'react';
+import Resultado from "./Resultado";
+import axios from 'axios'
+import ls from 'local-storage'
+import Host from '../Utilities/ServerUtilities'
+import {Link} from 'react-router-dom';
+
+
+class PriceRange extends Component {
     constructor(props) {
         super(props);
 
-        this.state = {
-            value: { min: 2, max: 10 },
-        };
+    }
+
+    state = {
+        states: [],
+        minValue: "",
+        maxValue: ""
+
+    }
+
+    async minValue(event) {
+        this.setState({minValue: event.target.value})
+    }
+
+    async maxValue(event) {
+        this.setState({maxValue: event.target.value});
     }
 
     render() {
         return (
-            <InputRange
-                maxValue={20}
-                minValue={0}
-                value={this.state.value}
-                onChange={value => this.setState({ value })} />
+            <div className="container p-4 col-md-7 col-sm-10">
+                <form className="form-inline my-2 my-lg-0">
+                    <input className="form-control mr-sm-2" type="search"
+                           value={this.state.minValue}
+                           onChange={this.minValue.bind(this)}
+                           placeholder="Precio Menor"
+                           aria-label="Search"
+
+                    >
+                    </input>
+                    <input className="form-control mr-sm-2" type="search"
+                           value={this.state.maxValue}
+                           onChange={this.maxValue.bind(this)}
+                           placeholder="Precio Mayor"
+                           aria-label="Search"
+
+                    >
+                    </input>
+                    <Link className="btn btn-primary btn-block" onClick={this.onclick}
+                          to={{
+                              pathname: '/ProductsByPriceSearch',
+                              state: {minValue: this.state.minValue, maxValue: this.state.maxValue}
+                          }}>Buscar Productos</Link>
+                </form>
+            </div>
+
         );
     }
 }
 
 export default PriceRange;
+/*
+import React, {Component} from 'react';
+
+class Search extends Component {
+
+    searchRef = React.createRef();
+getDatas = (e) => {
+    e.preventDefault();
+
+
+    this.props.dataSearch(this.searchRef.current.value);
+}
+
+render() {
+    return (
+        <form onSubmit={this.getDatas}>
+            <div className="row">
+
+                <div className="form-group col-md-8">
+                    <input ref={this.searchRef} type="text" className="form-control from-control-lg"
+                           placeholder="Buscar producto"/>
+                </div>
+
+                <div className="form-group col-md-4">
+                    <input type="submit" className="btn btn-lg btn-block" value="Buscar"/>
+                </div>
+            </div>
+        </form>
+    );
+}
+
+}
+
+
+export default Search;
+ */
